811ffcd19058ecf29f39a6e392ac97667e04176c
Adrian55Stack <adrianciurea112@yahoo.com>
2025-12-04 19:38:58 +0200

    add status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { ChatHeaderComponent } from './chat-header/chat-header.component';
 import { ChatFooterComponent } from './chat-footer/chat-footer.component';
 import { ChatContentComponent } from './chat-content/chat-content.component';
