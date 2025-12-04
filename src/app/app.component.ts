a23c98499da61bde45a7b47ab80f29fdf3ad3063
Adrian55Stack <adrianciurea112@yahoo.com>
2025-12-04 19:59:15 +0200

    add card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { ChatHeaderComponent } from './chat-header/chat-header.component';
 import { ChatFooterComponent } from './chat-footer/chat-footer.component';
 import { ChatContentComponent } from './chat-content/chat-content.component';
-
+import {MatCardModule} from '@angular/material/card';
 @Component({
   selector: 'app-root',
-  imports: [ChatHeaderComponent, ChatContentComponent, ChatFooterComponent],
+  imports: [ChatHeaderComponent, ChatContentComponent, ChatFooterComponent, MatCardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
