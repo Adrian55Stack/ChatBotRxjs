5114d54396ceff066e587d34619568d6d1cfcff0
Adrian55Stack <adrianciurea112@yahoo.com>
2025-12-04 19:36:51 +0200

    add basic structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ChatHeaderComponent } from './chat-header/chat-header.component';
+import { ChatFooterComponent } from './chat-footer/chat-footer.component';
+import { ChatContentComponent } from './chat-content/chat-content.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ChatHeaderComponent, ChatContentComponent, ChatFooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
