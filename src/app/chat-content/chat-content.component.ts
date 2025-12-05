321618dfa56b5a22552288d16dd2fdae68a62c80
Adrian55Stack <adrianciurea112@yahoo.com>
2025-12-05 14:27:40 +0200

    add basic expansion

--- src/app/chat-content/chat-content.component.ts
+++ src/app/chat-content/chat-content.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import {MatExpansionModule} from '@angular/material/expansion';
+import { IMessage } from '../models/message.model';
 
 @Component({
   selector: 'app-chat-content',
-  imports: [],
+  imports: [MatExpansionModule],
   templateUrl: './chat-content.component.html',
   styleUrl: './chat-content.component.scss'
 })
 export class ChatContentComponent {
-
+  messages: IMessage[] =[];
 }
