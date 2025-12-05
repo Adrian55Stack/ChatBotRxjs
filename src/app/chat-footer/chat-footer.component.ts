6f82853e3b01037e8cdebbf6d74698331676c702
Adrian55Stack <adrianciurea112@yahoo.com>
2025-12-05 14:42:47 +0200

    add input logic and message service

--- src/app/chat-footer/chat-footer.component.ts
+++ src/app/chat-footer/chat-footer.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon'
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MessagesService } from '../services/messages.service';
 
 @Component({
   selector: 'app-chat-footer',
-  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule, ReactiveFormsModule],
   templateUrl: './chat-footer.component.html',
   styleUrl: './chat-footer.component.scss'
 })
-export class ChatFooterComponent {
+export class ChatFooterComponent{
+  messageService = inject(MessagesService);
 
+  input: FormControl = new FormControl();
+
+  sendMessage() {
+
+  }
 }
