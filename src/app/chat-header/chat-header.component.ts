c0984faf84b6085821543588a261236b019acc9a
Adrian55Stack <adrianciurea112@yahoo.com>
2025-12-06 16:27:17 +0200

    add logic for bot availability

--- src/app/chat-header/chat-header.component.ts
+++ src/app/chat-header/chat-header.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BotService } from '../services/bot.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-chat-header',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './chat-header.component.html',
   styleUrl: './chat-header.component.scss'
 })
-export class ChatHeaderComponent {
+export class ChatHeaderComponent implements OnInit {
+  botService = inject(BotService);
+  availability: Observable<boolean>;
 
+  ngOnInit(): void {
+    this.availability = this.botService.getBotAvailability();
+  }
 }
