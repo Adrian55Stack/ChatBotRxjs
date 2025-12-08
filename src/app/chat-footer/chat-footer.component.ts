bb035040af7261f44719f2526039a7fde7a020ce
Adrian55Stack <adrianciurea112@yahoo.com>
2025-12-08 17:10:31 +0200

    include debounceTime and async pipe on disabled button

--- src/app/chat-footer/chat-footer.component.ts
+++ src/app/chat-footer/chat-footer.component.ts
@@ -6,21 +6,38 @@ import {MatIconModule} from '@angular/material/icon'
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MessagesService } from '../services/messages.service';
 import { CoreMessageService } from '../services/core-message.service';
+import { BehaviorSubject, concatMap, debounceTime, Observable, of, Subject } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-chat-footer',
-  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule, ReactiveFormsModule],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule, ReactiveFormsModule, AsyncPipe],
   templateUrl: './chat-footer.component.html',
   styleUrl: './chat-footer.component.scss'
 })
-export class ChatFooterComponent{
+export class ChatFooterComponent implements OnInit {
   coreMessageService = inject(CoreMessageService);
 
   input: FormControl = new FormControl();
 
+  isDisabled$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
+
+  ngOnInit(): void {
+    this.listenToValueChanges();
+  }
+
+  listenToValueChanges() {
+    this.input.valueChanges.pipe(
+      debounceTime(1000)
+    ).subscribe(val => {
+      this.isDisabled$.next(!val);
+    });
+  }
+
   sendMessage() {
     this.coreMessageService.setMessage(this.input.value);
     this.input.reset();
+    this.isDisabled$.next(true);
   }
 
   endConversation() {
