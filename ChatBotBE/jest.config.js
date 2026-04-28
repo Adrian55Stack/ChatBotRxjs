bcce924e55a7a041f04f6692c56f0c0db79ccea0
Adrian55Stack <adrianciurea112@yahoo.com>
2026-04-28 21:02:14 +0300

    add jest config separately

---
+++ ChatBotBE/jest.config.js
@@ -0,0 +1,21 @@
+export default {
+    "testEnvironment": "node",
+    "collectCoverageFrom": [
+      "src/**/*.js",        
+      "!src/**/*.spec.js"   
+    ],
+    "coverageReporters": [
+      "lcov",
+      "text",
+      "html"
+    ],
+    "coverageDirectory": "coverage",
+    "coverageThreshold": {
+      "global": {
+        "lines": 80,
+        "functions": 80,
+        "branches": 80,
+        "statements": 80
+      }
+    }
+  }
