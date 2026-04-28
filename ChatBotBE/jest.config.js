d3203bf20df689b33e661659355f949c57d4c607
Adrian55Stack <adrianciurea112@yahoo.com>
2026-04-28 21:03:09 +0300

    beautify files

--- ChatBotBE/jest.config.js
+++ ChatBotBE/jest.config.js
@@ -1,21 +1,21 @@
 export default {
     "testEnvironment": "node",
     "collectCoverageFrom": [
-      "src/**/*.js",        
-      "!src/**/*.spec.js"   
+        "src/**/*.js",
+        "!src/**/*.spec.js"
     ],
     "coverageReporters": [
-      "lcov",
-      "text",
-      "html"
+        "lcov",
+        "text",
+        "html"
     ],
     "coverageDirectory": "coverage",
     "coverageThreshold": {
-      "global": {
-        "lines": 80,
-        "functions": 80,
-        "branches": 80,
-        "statements": 80
-      }
+        "global": {
+            "lines": 80,
+            "functions": 80,
+            "branches": 80,
+            "statements": 80
+        }
     }
-  }
+}
