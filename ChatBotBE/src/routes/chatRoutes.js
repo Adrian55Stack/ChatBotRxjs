7828d6e9d861e54f3837e1f7acbedcfe0f2c0c02
Adrian55Stack <adrianciurea112@yahoo.com>
2026-05-30 21:44:44 +0300

    chore: add ChatBotBE as submodule

--- ChatBotBE/src/routes/chatRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { sendMessage } from '../controllers/chatController.js';
-
-const router = Router();
-
-router.post('/chat', sendMessage);
-
-export default router;
